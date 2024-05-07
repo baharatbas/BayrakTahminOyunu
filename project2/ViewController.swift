cfe34ab07456cae35c3658ac98d4fe42719e5334
baharatbas <baharatbas102@gmail.com>
2024-05-07 16:31:24 +0300

    Initial Commit

---
+++ project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project2
+//
+//  Created by Bahar Atbaş on 7.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
